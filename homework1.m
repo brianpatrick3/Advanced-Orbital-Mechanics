6b9fcc9a7d34b6b733a36c3fc2cab8a8691462de
brianpatrick3 <briancp2@illinois.edu>
2023-02-06 12:12:18 -0700

    initial commit
    Created Repository and uploaded homework 1 pdf along with my folder for homework 1 containing the necessary work.

---
+++ homework1.m
@@ -0,0 +1,36 @@
+%%% File Name: homework1.m 
+%  Written By: Brian Patrick 
+%  Date Written: 2.6.2023 
+%  Description: This file is used to complete the assigned work for
+%               homework 1 in AE 502 - Advanced Orbital Mechanics 
+%  ========================================================================
+close all; clear; clc; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
